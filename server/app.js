24373a956c50ae6a8f221e18600ca6ddbf10682d
Rodwell <rrodwell@deloitte.com>
2019-09-30 10:28:21 -0400

    connecting mongoDB and creating db schemas

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://ryan:test1234@graphqlplaylist-piu9f.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.once('open', () => {
+  console.log('Connected to database.')
+})
+
 app.use('/graphql', graphqlHTTP({
-  schema,
+  schema, //Defining graphql graph and object types on that graph
   graphiql: true
 }));
 
