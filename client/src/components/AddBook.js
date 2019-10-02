a2ad17195ca52cbbb0084a940b7fd155bc193036
Rodwell <rrodwell@deloitte.com>
2019-10-02 00:02:44 -0400

    moving queries into separate file

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,16 +1,6 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`
-
+import { getAuthorsQuery } from '../queries/queries';
 class AddBook extends React.Component {
   
   displayAuthors(){
